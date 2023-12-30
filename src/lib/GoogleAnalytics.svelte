b45e9c97b7d92d7ff42cb9750fda4f21ef5598ae
kamal <dhimank079@gmail.com>
2023-12-30 08:30:16 +0530

    update 2.0.2

--- src/lib/GoogleAnalytics.svelte
+++ src/lib/GoogleAnalytics.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+
 	export let trackingId = '';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', trackingId, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+	
 </script>
 
 <svelte:head>
 	{#if trackingId}
-		<script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}></script>
+		<script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}>
+		</script>
 		<script>
 			window.dataLayer = window.dataLayer || [];
+
 			function gtag() {
 				dataLayer.push(arguments);
 			}
-			gtag('js', new Date());
 
+			gtag('js', new Date());
 			gtag('config', trackingId);
 		</script>
 	{/if}
